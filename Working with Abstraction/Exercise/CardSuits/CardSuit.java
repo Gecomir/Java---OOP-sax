04db4aaf2a1354bf6190d47e3da3a4286153a7da
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-07-01 19:58:50 +0300

    Add files via upload

---
+++ Working with Abstraction/Exercise/CardSuits/CardSuit.java
@@ -0,0 +1,17 @@
+package WorkingWithAbstraction.CardSuit;
+
+public enum CardSuit {
+    CLUBS(0),
+    DIAMONDS(13),
+    HEARTS(26),
+    SPADES(39);
+    private  int suitPower;
+
+    public int getSuitPower() {
+        return suitPower;
+    }
+
+    CardSuit(int suitPower) {
+        this.suitPower = suitPower;
+    }
+}
