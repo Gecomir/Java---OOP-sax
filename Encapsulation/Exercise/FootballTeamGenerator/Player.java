fecb64cc92b9d48ae6dca013c1e55c213c48e35c
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-07-05 17:11:22 +0300

    Add files via upload

---
+++ Encapsulation/Exercise/FootballTeamGenerator/Player.java
@@ -0,0 +1,66 @@
+package Encapsulation.FootballTeamGenerator;
+
+public class Player {
+    private String name;
+    private int endurance;
+    private int sprint;
+    private int dribble;
+    private int passing;
+    private int shooting;
+
+    public Player(String name, int endurance, int sprint, int dribble, int passing, int shooting) {
+        setName(name);
+        setEndurance(endurance);
+        setSprint(sprint);
+        setDribble(dribble);
+        setPassing(passing);
+        setShooting(shooting);
+
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("A name should not be empty");
+        }
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setEndurance(int endurance) {
+        checkStats(endurance, "Endurance");
+        this.endurance = endurance;
+    }
+
+    private void setSprint(int sprint) {
+        checkStats(sprint, "Sprint");
+        this.sprint = sprint;
+    }
+
+    private void setDribble(int dribble) {
+        checkStats(dribble, "Dribble");
+        this.dribble = dribble;
+    }
+
+    private void setPassing(int passing) {
+        checkStats(passing, "Passing");
+        this.passing = passing;
+    }
+
+    private void setShooting(int shooting) {
+        checkStats(shooting, "Shooting");
+        this.shooting = shooting;
+    }
+
+    private void checkStats(int stats, String statsName) {
+        if (stats < 0 || stats > 100) {
+            throw new IllegalArgumentException(statsName + " should be between 0 and 100.");
+        }
+    }
+
+    public double overallSkillLevel() {
+        return (endurance + sprint + dribble + passing + shooting) / 5.0;
+    }
+}
