c07572d575090305f40efb8ec89f811b1706ff70
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-07-07 19:02:11 +0300

    Add files via upload

---
+++ Inheritance/Exercise/PlayersAndMonsters/Main.java
@@ -0,0 +1,10 @@
+package hero;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        BladeKnight bladeKnight = new BladeKnight("Gecomir", 99);
+        System.out.println(bladeKnight);
+    }
+}
