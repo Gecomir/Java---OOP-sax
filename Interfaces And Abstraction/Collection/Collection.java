d152eaa3d175ac2be33065d5ec9e991e19e4a66f
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-07-14 13:48:20 +0300

    Add files via upload

---
+++ Interfaces And Abstraction/Collection/Collection.java
@@ -0,0 +1,14 @@
+package InterfacesAndAbstraction.Collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Collection {
+    protected int maxSize;
+    protected List<String> items;
+
+    public Collection() {
+        maxSize = 100;
+        items = new ArrayList<>();
+    }
+}
