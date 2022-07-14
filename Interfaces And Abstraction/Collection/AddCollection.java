71b130fb054b00308d9180b978e47f1ee660fb5f
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-07-14 13:47:19 +0300

    Add files via upload

---
+++ Interfaces And Abstraction/Collection/AddCollection.java
@@ -0,0 +1,10 @@
+package InterfacesAndAbstraction.Collection;
+
+public class AddCollection extends Collection implements Addable{
+
+    @Override
+    public int add(String element) {
+        items.add(element);
+        return items.indexOf(element);
+    }
+}
