fffcab463b388ce86e883d85291bf81adea2b74e
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-07-15 14:21:00 +0300

    Add files via upload

---
+++ Polymorphism/Exercises/Vehicles/Messages.java
@@ -0,0 +1,7 @@
+package Polymorphism.Vehicles;
+
+public class Messages {
+
+    public static final String TRAVELING = "%s travelled %s km";
+    public static final String NOT_TRAVELING = "%s needs refueling";
+}
