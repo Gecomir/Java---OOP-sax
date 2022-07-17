8a7eaa29b11293b60bd5d145ea5e2f8a157abb33
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-07-17 11:09:38 +0300

    Add files via upload

---
+++ Polymorphism/Exercises/WildFarm/Animal/Mouse.java
@@ -0,0 +1,26 @@
+package Polymorphism.WildFarm.Animal;
+
+
+import Polymorphism.WildFarm.Food.Food;
+import Polymorphism.WildFarm.Food.Meat;
+import Polymorphism.WildFarm.Messages;
+
+public class Mouse extends Mammal {
+
+    public Mouse(String animalName, String animalType, Double animalWeight, String livingRegion) {
+        super(animalName, animalType, animalWeight, livingRegion);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(Messages.MOUSE_SOUND);
+    }
+
+    @Override
+    public void eat(Food food) {
+        if(food instanceof Meat){
+            throw new IllegalArgumentException(String.format(Messages.WRONG_FOOD, "Mice"));
+        }
+        super.eat(food);
+    }
+}
