866ade245df0cf0ec5422af6e88fce0799d3cc94
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-07-18 14:58:21 +0300

    Delete Extensions.java

--- Polymorphism/Exercises/Calculator/Extensions.java
+++
@@ -1,7 +0,0 @@
-package Polymorphism.Calculator;
-
-public class Extensions {
-    public static InputInterpreter buildInterpreter(CalculationEngine engine) {
-        return new InputInterpreter(engine);
-    }
-}
