4d6d7aa54affed27657e86f226e479b37aa56e5f
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-07-23 14:45:32 +0300

    Add files via upload

---
+++ Reflection and Annotation/Exercises/BarracksWars - Return of the Dependencies/interfaces/CommandInterpreter.java
@@ -0,0 +1,8 @@
+package barracksWars.interfaces;
+
+public interface CommandInterpreter {
+
+	Executable interpretCommand(String[] data, String commandName);
+
+    Executable interpretCommand(String[] data);
+}
