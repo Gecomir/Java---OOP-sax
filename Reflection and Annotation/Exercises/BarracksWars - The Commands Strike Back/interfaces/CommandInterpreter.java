14146407d5f83ffac84800bede988f837e35c6dc
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-07-30 18:12:19 +0300

    Delete CommandInterpreter.java

--- Reflection and Annotation/Exercises/BarracksWars - The Commands Strike Back/interfaces/CommandInterpreter.java
+++
@@ -1,6 +0,0 @@
-package ReflectionAndAnnotation.BarracksWars_TheCommandsStrikeBack.barracksWars.interfaces;
-
-public interface CommandInterpreter {
-
-	Executable interpretCommand(String[] data, String commandName);
-}
