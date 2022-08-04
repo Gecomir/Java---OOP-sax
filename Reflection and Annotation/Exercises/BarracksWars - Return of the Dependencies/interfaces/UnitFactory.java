5d2f645d02a7c901a17a3fbaf2744703bd11c216
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-08-04 19:17:53 +0300

    Add files via upload

---
+++ Reflection and Annotation/Exercises/BarracksWars - Return of the Dependencies/interfaces/UnitFactory.java
@@ -0,0 +1,10 @@
+package ReflectionAndAnnotation.barracksWarsDependencies.interfaces;
+
+import jdk.jshell.spi.ExecutionControl;
+
+import java.lang.reflect.InvocationTargetException;
+
+public interface UnitFactory {
+
+    Unit createUnit(String unitType) throws ExecutionControl.NotImplementedException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException;
+}
