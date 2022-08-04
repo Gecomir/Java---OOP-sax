08ef4c56b94c19fe7f8edef6b3a7899c8d0ab40e
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-08-04 19:11:29 +0300

    Add files via upload

---
+++ Reflection and Annotation/Exercises/BarracksWars - Return of the Dependencies/core/factories/UnitFactoryImpl.java
@@ -0,0 +1,33 @@
+package ReflectionAndAnnotation.barracksWarsDependencies.core.factories;
+
+import ReflectionAndAnnotation.barracksWarsDependencies.interfaces.Unit;
+import ReflectionAndAnnotation.barracksWarsDependencies.interfaces.UnitFactory;
+import jdk.jshell.spi.ExecutionControl;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class UnitFactoryImpl implements UnitFactory {
+
+    private static final String UNITS_PACKAGE_NAME =
+            "barracksWars.models.units.";
+
+    @Override
+    @SuppressWarnings("all")
+    public Unit createUnit(String unitType) throws ExecutionControl.NotImplementedException, ClassNotFoundException,
+            NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        final Class<Unit> unitClass = (Class<Unit>) Class.forName(UNITS_PACKAGE_NAME + unitType);
+
+       return createUnit(unitClass);
+
+    }
+
+    private Unit createUnit(Class<Unit> unitClass) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+       final Constructor<Unit> ctors = unitClass.getDeclaredConstructor();
+       ctors.setAccessible(true);
+
+       return ctors.newInstance();
+    }
+
+}
