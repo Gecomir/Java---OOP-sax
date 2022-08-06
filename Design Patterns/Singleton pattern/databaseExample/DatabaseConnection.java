97358abf215d17b8c89f03c8ecab1409ec3fe6b1
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-08-06 14:55:01 +0300

    Add files via upload

---
+++ Design Patterns/Singleton pattern/databaseExample/DatabaseConnection.java
@@ -0,0 +1,28 @@
+package DesignPatterns.singleton.databaseExample;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+
+    private DatabaseConnection() {
+        try {
+            Thread.sleep(5_000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public void readData() {
+        System.out.println("Reading from DB");
+    }
+
+    public void writeData() {
+        System.out.println("Writing to DB");
+    }
+}
