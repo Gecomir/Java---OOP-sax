2473205554995383ac3b460c5308c48eab9272b9
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-08-06 15:03:33 +0300

    Add files via upload

---
+++ Design Patterns/Factory pattern/factoryExercise/cake/WhiteCake.java
@@ -0,0 +1,23 @@
+package DesignPatterns.factoryExercise.cake;
+
+public class WhiteCake extends Cake {
+
+    public WhiteCake(double diameter, double price, int pieces) {
+        super(diameter, price, pieces);
+    }
+
+    @Override
+    public void prepare() {
+
+    }
+
+    @Override
+    public void bake() {
+
+    }
+
+    @Override
+    public void box() {
+
+    }
+}
