1cf3489cc85fceaee79b76a2576a83a482e26f3a
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-08-07 12:49:43 +0300

    Add files via upload

---
+++ Design Patterns/Proxy pattern/LionProxy.java
@@ -0,0 +1,16 @@
+package DesignPatterns.proxyPattern;
+
+public class LionProxy implements Animal {
+    Lion lion;
+
+    public LionProxy() {
+        this.lion = new Lion();
+    }
+
+    @Override
+    public void speak() {
+        System.out.println("Before speak method from parent");
+        lion.speak();
+        System.out.println("After speak method from parent");
+    }
+}
