aa56dbebd30afa962fdc2f7f871d0c54faf178e3
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-08-07 12:11:16 +0300

    Add files via upload

---
+++ Design Patterns/Command pattern/VolumeUpCommand.java
@@ -0,0 +1,17 @@
+package DesignPatterns.commandPattern;
+
+public class VolumeUpCommand implements Command {
+    private TVRemote tvRemote;
+
+    public VolumeUpCommand(TVRemote tvRemote) {
+        this.tvRemote = tvRemote;
+    }
+
+    @Override
+    public void execute() {
+        int oldVolume = tvRemote.getVolume();
+        tvRemote.setVolume(++oldVolume);
+        System.out.println("New volume is " + tvRemote.getVolume());
+
+    }
+}
