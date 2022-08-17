2b3c49e21ec0252e044efb90b2f57d9f1962f69b
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-08-17 21:18:18 +0300

    Add files via upload

---
+++ Examp preparation/Exam - 14 August 2022/football/entities/player/Player.java
@@ -0,0 +1,15 @@
+package football.entities.player;
+
+public interface Player {
+    void setName(String name);
+
+    int stimulation();
+
+    double getKg();
+
+    String getName();
+
+    int getStrength();
+
+
+}
