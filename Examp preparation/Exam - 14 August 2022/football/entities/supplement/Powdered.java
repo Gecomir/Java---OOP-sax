5186487070cad3e699ffbf30a6822626f89663f0
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-08-17 21:19:53 +0300

    Add files via upload

---
+++ Examp preparation/Exam - 14 August 2022/football/entities/supplement/Powdered.java
@@ -0,0 +1,11 @@
+package football.entities.supplement;
+
+public class Powdered extends BaseSupplement{
+
+    private static int energy = 120;
+    private static double price = 15;
+
+    public Powdered() {
+        super(energy, price);
+    }
+}
