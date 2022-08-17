ecc13190920b508205962dabb7f6f62a5f672e3e
Gecomir <102181356+Gecomir@users.noreply.github.com>
2022-08-17 21:19:35 +0300

    Add files via upload

---
+++ Examp preparation/Exam - 14 August 2022/football/entities/supplement/Liquid.java
@@ -0,0 +1,11 @@
+package football.entities.supplement;
+
+public class Liquid extends BaseSupplement{
+
+    private static int energy = 90;
+    private static double price = 25;
+
+    public Liquid() {
+        super(energy, price);
+    }
+}
